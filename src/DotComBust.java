b6f742e6f0cd9d45201d285bf416107dd83113fb
danikhani <30578398+danikhani@users.noreply.github.com>
2019-10-23 22:01:10 +0900

    Start the game method got added to DotComBust

--- src/DotComBust.java
+++ src/DotComBust.java
@@ -1,5 +1,30 @@
 //page 144 book.
+import java.util.*
+
 public class DotComBust {
-    Gamehelper helper = new Gamehelper();
-    Arraylist
+    private Gamehelper helper = new Gamehelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame(){
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Your goal is to sink three dot coms.");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+
 }
